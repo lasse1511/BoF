7a057efa13ce8e5bc558d0fd02c8c5bea0dbf053
lasse1511 <lassebonner@Lasses-Mac.local>
2017-02-17 13:51:46 +0100

    First side go connections melee de to andre sider

--- BoF/ViewController.swift
+++ BoF/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var gamename: String!
+    
+    @IBOutlet var MainView: UIView!
+    @IBOutlet var MainPopUp: UIView!
+    @IBOutlet var JoinPopup: UIView!
+    
+    @IBOutlet var FrontView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        MainPopUp.isHidden = true
+        JoinPopup.isHidden = true
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func Click(_ sender: UIButton) {
+        FrontView.alpha = 0.3
+        JoinPopup.isHidden = false
+    }
 
 
+    @IBAction func CreateClick(_ sender: UIButton) {
+        FrontView.alpha = 0.3
+        MainPopUp.isHidden = false
+    }
 }
 
