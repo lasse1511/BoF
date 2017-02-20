31a4b1438d7a70de0859e60cb04de6b77451a730
danielbuhl <daniel@buhl.nu>
2017-02-20 11:53:43 +0100

    Set TimePicker

--- BoF/SetupViewController.swift
+++ BoF/SetupViewController.swift
@@ -8,48 +8,42 @@
 
 import UIKit
 
-class SetupViewController: UIViewController/*, UIPickerViewDelegate, UIPickerViewDataSource */{
+class SetupViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
-    @IBOutlet weak var CDTimer: UIDatePicker!
+
+
+
+
+
     @IBOutlet weak var RoundsTable: UITableView!
     @IBOutlet weak var TimePicker: UIPickerView!
     
-    var TimePickerData: [[Int]] = [[Int]]()
+    var TimePickerData = [[0,1,2],[0,5,10,15,20,25,30,35,40,45,50,55]]
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        //self.TimePicker.delegate = self
-        //self.TimePicker.dataSource = self
         
+        self.TimePicker.delegate = self
+        self.TimePicker.dataSource = self
         
-        TimePickerData = [[0,1,2],[0,5,10,15,20,25,30,35,40,45,50,55]]
+            
 
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    // The number of columns of data
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2
     }
-    
-    // The number of rows of data
-    func UIPickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return TimePickerData.count
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return TimePickerData[component].count
     }
     
-    /* The data to return for the row and component (column) that's being passed in
-    func UIPickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return TimePickerData[component][row]
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(TimePickerData[component][row])
     }
 
-    */
+    
     /*
     // MARK: - Navigation
 
