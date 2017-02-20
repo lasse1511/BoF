de38b5fa1f1b8440af8e4c5aef8d7024b85c497d
lasse1511 <lassebonner@Lasses-Mac.local>
2017-02-20 11:51:49 +0100

    bla bla

---
+++ BoF/DTO.swift
@@ -0,0 +1,15 @@
+//
+//  DTO.swift
+//  BoF
+//
+//  Created by Lasse Bonner on 20/02/2017.
+//  Copyright © 2017 Lasse Bonner. All rights reserved.
+//
+
+import Foundation
+
+
+struct gameDTO {
+    static var gameName = ""
+    
+}
